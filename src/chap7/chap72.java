1a1f165db63789a67f3aae0060494ccc234eda1f
gavrishevs <gavrishevs@m90-gavrishevs.office.ctco.lv>
2011-09-23 17:59:46 +0300

    binarySearch, toArray, Sort array, SortBy comparator

--- src/chap7/chap72.java
+++ src/chap7/chap72.java
@@ -1,12 +1,13 @@
 package chap7;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-class Humanis implements Comparable<Humanis>{
+class Humanis {
 	private String name;
 	private String nick;
 	private Integer age;
@@ -37,7 +38,6 @@ class Humanis implements Comparable<Humanis>{
 		this.age = age;
 	}
 
-
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -50,15 +50,21 @@ class Humanis implements Comparable<Humanis>{
 		builder.append("]");
 		return builder.toString();
 	}
+}
 
+class BigHumanis extends Humanis implements Comparable<Humanis>{
+	
+	public BigHumanis(String name, String nick, Integer age) {
+		super(name, nick, age);
+	}
 
 	@Override
 	public int compareTo(Humanis o) {
-		if (this.age < o.getAge())
+		if (this.getAge() < o.getAge())
 			return -1;
-		else if (this.age > o.getAge())
+		else if (this.getAge() > o.getAge())
 			return 1;
-		else if (this.age == o.getAge())
+		else if (this.getAge() == o.getAge())
 			return 0;
 		return 0;
 	}
@@ -76,6 +82,13 @@ class CompareByNik implements Comparator<Humanis>{
 	}
 }
 
+class CompareDesc implements Comparator<String>{
+	@Override
+	public int compare(String o1, String o2) {
+		return (o1.compareTo(o2) * (-1));
+	}
+}
+
 
 public class chap72 {
 
@@ -86,18 +99,107 @@ public class chap72 {
 	public chap72() {
 		//chap721();
 		//chap722();		
+		//chap723();
+		//chap724();
+		//chap725();
+		chap726();
+
+	}
+	
+	/**
+	 * Array binarySearch
+	 */
+	public void chap726(){
 		
+		String[] list = {"ccc", "aaa","ggg","bbb","111"};
+		
+		for (String str : list )
+			System.out.print( str +" ");
+		System.out.println();
+		
+		//If you attempt to search an array or collection that has not already been
+		// sorted, the results of the search will not be predictable !!!!!!
+		int poz = Arrays.binarySearch(list, "ggg");
+		System.out.println("poz : " + poz + " : "+ list[poz] );
+		
+		// simple lex sort and search
+		Arrays.sort(list);
+		for (String str : list )
+			System.out.print( str +" ");
+		System.out.println();
+		poz = Arrays.binarySearch(list, "ggg");
+		System.out.println("poz : " + poz + " : "+ list[poz] );
+
+		// sort desc and search 
+		CompareDesc cd = new CompareDesc();
+		Arrays.sort(list, cd);
+		for (String str : list )
+			System.out.print( str +" ");
+		System.out.println();
+		poz = Arrays.binarySearch(list, "ggg", cd);
+		System.out.println("poz : " + poz + " : "+ list[poz] );
+		
+		
+	}
+	
+	/**
+	 * List To array and back  , List.toArray, Arrays.asList
+	 */
+	public void chap725(){
+		//List
+		List<String> list = new ArrayList<String>();
+		list.add("bbb");
+		list.add("ccc2");
+		list.add("ddd");		
+		
+		// To array 
+		System.out.println(list);
+		Object[] arrayList = list.toArray();
+
+		for (Object obj : arrayList)
+			System.out.print( " "+ obj );
+		System.out.println();
+		
+		//To List
+		List mylist= Arrays.asList(arrayList);
+		System.out.println(mylist);
+	}
+	
+	/**
+	 * Sort Array  
+	 */
+	public void chap724(){
+		String[] myList = {"zzz", "ccc", "bbb", "111", "111" };
+		
+		for (String str : myList)
+			System.out.print( " "+ str);
+		System.out.println( " ");
+		
+		Arrays.sort(myList);
+		
+		for (String str : myList)
+			System.out.print( " "+ str);
+		System.out.println( " ");		
+		
+		CompareDesc cd =  new  CompareDesc();		
+		Arrays.sort(myList, cd);
+		
+		for (String str : myList)
+			System.out.print( " "+ str);
+		System.out.println( " ");		
+
 	}
 	
 	/**
 	 * List Object , SortBy  Comparator 
 	 */
-	public void chap722(){
-		ArrayList<Humanis> piiplli = new ArrayList<Humanis>();
-		piiplli.add(new Humanis("vasa",  "vasok",  21));
-		piiplli.add(new Humanis("sanna", "asnnok", 22));
-		piiplli.add(new Humanis("gera",  "gerok",  19));		
+	public void chap723(){
+		List<BigHumanis> piiplli = new ArrayList<BigHumanis>();
+		piiplli.add(new BigHumanis("vasa",  "vasok",  21));
+		piiplli.add(new BigHumanis("sanna", "asnnok", 22));
+		piiplli.add(new BigHumanis("gera",  "gerok",  19));		
 		
+		// sort by age 
 		System.out.println(" before sort " + piiplli);
 		Collections.sort(piiplli);
 		System.out.println(" after  sort " + piiplli);
@@ -111,9 +213,24 @@ public class chap72 {
 		CompareByNik cbnc = new CompareByNik();
 		Collections.sort(piiplli, cbnc);
 		System.out.println("SortByNick : " + piiplli);
+		
+		
 	}
 	    
-	
+	/**
+	 * Simple object sort  
+	 */
+	public void chap722(){
+		List<Humanis> piiplli = new ArrayList<Humanis>();
+		piiplli.add(new Humanis("vasa",  "vasok",  21));
+		piiplli.add(new Humanis("sanna", "asnnok", 22));
+		piiplli.add(new Humanis("gera",  "gerok",  19));		
+		System.out.println(" before sort " + piiplli);
+
+		//not working nead Comparable implament
+		//Collections.sort(piiplli);
+		System.out.println(" after  sort " + piiplli);
+	}
 	
 	
 	/**
