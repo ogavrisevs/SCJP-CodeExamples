842f773c17ffb8069661482c4a2f2c32502684ce
gavrishevs <gavrishevs@m90-gavrishevs.office.ctco.lv>
2011-09-28 11:01:25 +0300

    Test

---
+++ src/question/Test.java
@@ -0,0 +1,33 @@
+package question;
+
+public class Test {
+
+	public static void main(String[] args) {
+		new Test();
+	}
+	public Test() {
+		
+	}
+
+}
+
+class A{}
+class B extends A{}
+class C extends B{}
+
+class TestC {
+	public A getIt(){
+		return new A();
+	}
+}
+
+class TestCC extends TestC{
+	//public C getIt(){
+	//	return new C();
+	//}
+	public A getIt(){
+		return new C();
+	}
+	
+	
+}
