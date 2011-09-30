a9dc04ec495ff798bc9274ed8535d867723784c1
gavrishevs <gavrishevs@m90-gavrishevs.office.ctco.lv>
2011-09-30 16:34:48 +0300

    Stupid Git !

---
+++ src/chap3/exam/question7/Bridge.java
@@ -0,0 +1,27 @@
+package chap3.exam.question7;
+
+public class Bridge {
+	public enum Suits {
+		CLUBS(20), DIAMONDS(20), HEARTS(30), SPADES(30), NOTRUMP(40) {
+			public int getValue(int bid) {
+				return ((bid - 1) * 30) + 40;
+			}
+		};
+		Suits(int points) {
+			this.points = points;
+		}
+
+		private int points;
+
+		public int getValue(int bid) {
+			return points * bid;
+		}
+	}
+
+	public static void main(String[] args) {
+		/*System.out.println(Suits.NOTRUMP.getBidValue(3));*/
+		
+		System.out.println(Suits.SPADES + " " + Suits.SPADES.points);
+		System.out.println(Suits.values());
+	}
+}
