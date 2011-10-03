c005e4dae19b30127679a96dad2deb4e1401f7d8
gavrishevs <gavrishevs@m90-gavrishevs.office.ctco.lv>
2011-10-03 12:05:24 +0300

    Left Right Shift

---
+++ src/chap3/leftShift.java
@@ -0,0 +1,19 @@
+package chap3;
+
+public class leftShift {
+
+	public static void main(String[] args) {
+
+		System.out.println("(9 << 0) "+ (9 << 0));		
+		System.out.println("(9 << 1) "+ (9 << 1));
+		System.out.println("(9 << 2) "+ (9 << 2));
+		System.out.println("(9 << 3) "+ (9 << 3));
+		
+		System.out.println("(72 >> 0) "+ (72 >> 0));		
+		System.out.println("(72 >> 1) "+ (72 >> 1));
+		System.out.println("(72 >> 2) "+ (72 >> 2));
+		System.out.println("(72 >> 3) "+ (72 >> 3));
+		
+	}
+
+}
