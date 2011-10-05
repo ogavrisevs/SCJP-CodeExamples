f773c66bfb7ef643dfb36d3e21e56355a5e746ca
gavrishevs <gavrishevs@m90-gavrishevs.office.ctco.lv>
2011-10-05 17:56:42 +0300

    Sybex Testing var-arg

---
+++ src/sybex/AssessmentTest/Question1/Test.java
@@ -0,0 +1,12 @@
+package sybex.AssessmentTest.Question1;
+public class Test {
+	public static void main( String [] argv){
+		do1();
+	}
+
+	
+	static void do1(String... bb){
+		String [] aa = {"a", "b"};		
+	}
+	
+}
