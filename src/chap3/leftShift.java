7406b3152abfe197443f3c4fd455d5d9cb45b861
gavrishevs <gavrishevs@m90-gavrishevs.office.ctco.lv>
2011-10-10 18:03:26 +0300

    shift equal

--- src/chap3/leftShift.java
+++ src/chap3/leftShift.java
@@ -13,6 +13,13 @@ public class leftShift {
 		System.out.println("(72 >> 1) "+ (72 >> 1));
 		System.out.println("(72 >> 2) "+ (72 >> 2));
 		System.out.println("(72 >> 3) "+ (72 >> 3));
+
+		int i =9;
+		System.out.println("("+ i +" <<= 0) "+ (i <<= 0));		
+		System.out.println("("+ i +" <<= 1) "+ (i <<= 1));
+		System.out.println("("+ i +" <<= 2) "+ (i <<= 2));
+		System.out.println("("+ i +" <<= 3) "+ (i <<= 3));
+		
 	}
 
 }
